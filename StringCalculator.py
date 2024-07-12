02a426dd4847c57735f9d543d85d2a97fbcd1c6f
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-07-12 13:29:58 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,13 +1,20 @@
-def add(input):
-    if not input:
+def parse_input(input_string):
+    if input_string.startswith('//'):
+        delimiter = input_string[2]
+        return input_string[4:], delimiter
+    return input_string, ','
+
+def split_and_convert(input_string, delimiter):
+    return [int(num) for num in input_string.replace('\n', delimiter).split(delimiter)]
+
+def filter_numbers(numbers):
+    return [num for num in numbers if num <= 1000]
+
+def add(input_string):
+    if not input_string:
         return 0
-
-    delimiter = ','
-    if input.startswith('//'):
-        delimiter = input[2]
-        input = input[4:]
-    else:
-        input = input.replace('\n', delimiter)
-
-    inp = input.split(delimiter)
-    return sum(int(inp) for inp in inp if int(inp) <= 1000)
+    
+    parsed_input, delimiter = parse_input(input_string)
+    numbers = split_and_convert(parsed_input, delimiter)
+    filtered_numbers = filter_numbers(numbers)
+    return sum(filtered_numbers)
