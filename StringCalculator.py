0a2b44b48636105677f244813bcce24f2b1170de
Ganasai549 <73879939+Ganasai549@users.noreply.github.com>
2024-07-12 13:27:58 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,4 +1,13 @@
 def add(input):
-    if input == "":
-        return not 0
-   
+    if not input:
+        return 0
+
+    delimiter = ','
+    if input.startswith('//'):
+        delimiter = input[2]
+        input = input[4:]
+    else:
+        input = input.replace('\n', delimiter)
+
+    inp = input.split(delimiter)
+    return sum(int(inp) for inp in inp if int(inp) <= 1000)
